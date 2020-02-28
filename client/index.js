643c758bb9d7d9f2e3d5a4fc7a4e5de425d28561
msmedes <smichaelmedes@gmail.com>
2020-02-28 13:24:22 -0500

    file upload, conversion, transcription, transcription processing

--- client/index.js
+++ client/index.js
@@ -11,7 +11,7 @@ const handleSuccess = function (stream) {
   const options = { mimeType: 'audio/webm' };
   let recordedChunks = [];
   const mediaRecorder = new MediaRecorder(stream, options);
-  visualize(stream);
+  console.log(mediaRecorder);
 
   record.onclick = () => {
     mediaRecorder.start();
@@ -29,17 +29,19 @@ const handleSuccess = function (stream) {
   };
 
   mediaRecorder.ondataavailable = (e) => {
-    if (e.data.size > 0) {
-      recordedChunks.push(e.data);
-    }
+    console.log('e.data.size', e.data);
+    recordedChunks.push(e.data);
   };
 
   mediaRecorder.onstop = (e) => {
+    console.log('stream', stream.getTracks()[0].getSettings());
+    console.log('recordedChunks', recordedChunks);
     console.log('data available after MediaRecorder.stop() called.');
 
     const audio = document.createElement('audio');
     audio.controls = true;
-    const blob = new Blob(recordedChunks, { type: 'audio/ogg; codecs=opus' });
+    const blob = new Blob(recordedChunks, { type: 'audio/wav' });
+    handleAudioUpload(blob);
     recordedChunks = [];
     const audioURL = window.URL.createObjectURL(blob);
     audio.src = audioURL;
@@ -56,57 +58,19 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
   .then(handleSuccess, onError);
 
 
-function visualize(stream) {
-  if (!audioCtx) {
-    audioCtx = new AudioContext({ sampleRate: 48000 });
-  }
+const handleAudioUpload = (audio) => {
+  // const formData = new FormData();
+  // formData.append('file', audio, audio.name);
+  // console.log('formData', formData.getAll('file'));
 
-  const source = audioCtx.createMediaStreamSource(stream);
-
-  const analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 2048;
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array(bufferLength);
-
-  source.connect(analyser);
-  // analyser.connect(audioCtx.destination);
-
-  const draw = () => {
-    WIDTH = canvas.width;
-    HEIGHT = canvas.height;
-
-    requestAnimationFrame(draw);
-
-    analyser.getByteTimeDomainData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-    canvasCtx.beginPath();
-
-    const sliceWidth = WIDTH * 1.0 / bufferLength;
-    let x = 0;
-
-
-    for (let i = 0; i < bufferLength; i++) {
-      const v = dataArray[i] / 128.0;
-      const y = v * HEIGHT / 2;
-
-      if (i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-    }
-
-    draw();
-
-    canvasCtx.lineTo(canvas.width, canvas.height / 2);
-    canvasCtx.stroke();
-  };
-}
+  fetch('http://127.0.0.1:5000/transcribe', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/audio' },
+    body: audio,
+  })
+    .then((response) => response.json())
+    .then((data) => { console.log(data); })
+    .catch((error) => {
+      console.error(error);
+    });
+};
--- server/main.py
+++ server/main.py
@@ -1,148 +1,27 @@
-import time
-import datetime
-import json
+import os
+import ffmpeg
 import logging
-import threading
-import collections
-import queue
 import deepspeech
 import numpy as np
-import webrtcvad
-from halo import Halo
-from scipy import signal
-from flask import Flask
-from flask_socketio import SocketIO, emit
-from typing import Callable, Dict, Union, Callable
-
+import scipy.io.wavfile as wav
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from typing import Dict, Union
 logging.basicConfig(level=25)
 
 Config = Dict[str, Union[str, int, float]]
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'wav', 'mp3', 'flac', 'ogg'}
+
 app = Flask(__name__)
-socket = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
-
-# socket_buffer = collections.deque()
-
-
-@socket.on("connect", namespace="/transcribe")
-def connect_handler():
-    print("connected")
-    socket.emit("hello", {"data": "hello"})
-
-
-@socket.on("disconnect", namespace="/transcribe")
-def disconnect_handler():
-    print("disconnected")
-
-
-@socket.on("stream-data", namespace="/transcribe")
-def stream_reset_handler(stream):
-    buffer, timestamp = stream['buffer'], stream['timestamp']
-    # vad_audio.buffer_queue.put(buffer)
-    process_audio(buffer)
-
-
-@socket.on("stream-reset")
-def stream_reset_handler():
-    print('stream stopped')
-
-
-def emit_results(text: str):
-    print('emit results', text)
-    socket.emit("recognize", {"text": text}, namespace='/transcribe')
-
-
-class VADAudio:
-    """Filter and segment audio with voice activitiy detection."""
-    RATE_PROCESS = 16000
-    BLOCKS_PER_SECOND = 50
-
-    def __init__(self, aggressiveness=0, input_rate=RATE_PROCESS):
-        self.vad = webrtcvad.Vad(aggressiveness)
-        self.input_rate = input_rate
-        self.sample_rate = 16000
-        self.buffer_queue = queue.Queue()
-        self.block_size = int(self.RATE_PROCESS /
-                              float(self.BLOCKS_PER_SECOND))
-        self.block_size_input = int(
-            self.input_rate / float(self.BLOCKS_PER_SECOND))
-
-    frame_duration_ms = property(
-        lambda self: 1000 * self.block_size // self.sample_rate
-    )
-
-    def resample(self, data: str, input_rate: int) -> str:
-        """
-        Mic may not support our native processing sample rate, so
-        resample from input_rate to RATE_PROCESS here for webrtcvad and
-        deepspeech
-
-        Args:
-            data (binary): Input audio stream
-            input_rate (int): Input audio rate to resample from
-        """
-        data16 = np.frombuffer(data, dtype=np.int16)
-        resample_size = int(len(data16) / self.input_rate * self.RATE_PROCESS)
-        resample = signal.resample(data16, resample_size)
-        resample16 = np.array(resample, dtype=np.int16)
-        return resample16.tostring()
-
-    def read_resampled(self, frames) -> str:
-        """Return a block of audio data resampled to 16k, blocking if necessary"""
-        return self.resample(data=frames, input_rate=self.input_rate)
-
-    def read(self):
-        """Return a block of audio data, blocking if necessary"""
-        return self.buffer_queue.get()
-
-    def frame_generator(self, frames):
-        """Generator that yields all audio frames from a microphone"""
-        if self.input_rate == self.RATE_PROCESS:
-            # while True:
-            #     yield self.read()
-            yield frames
-
-        else:
-            # while True:
-            yield self.read_resampled(frames)
-
-    def vad_collector(self, padding_ms=150, ratio=0.9, frames=None):
-        """Generator that yields series of consecutive audio frames comprising each utterence, separated by yielding a single None.
-           Determines voice activity by ratio of frames in padding_ms. Uses a buffer to include padding_ms prior to being triggered.
-           Example: (frame, ..., frame, None, frame, ..., frame, None, ...)
-                     |---utterence---|        |---utterence---|
-       """        # if frames is None:
-        #     frames = self.frame_generator()
-        # else:
-
-        frames = self.frame_generator(frames)
-        num_padding_frames = padding_ms // self.frame_duration_ms
-        ring_buffer = collections.deque(maxlen=num_padding_frames)
-        triggered = False
-
-        for frame in frames:
-            if len(frame) < 640:
-                return
-            is_speech = self.vad.is_speech(frame, self.sample_rate)
-            if not triggered:
-                print('lol ikd')
-                ring_buffer.append((frame, is_speech))
-                num_voiced = len([f for f, speech in ring_buffer if speech])
-                if num_voiced > ratio * ring_buffer.maxlen:
-                    triggered = True
-                    for f, _ in ring_buffer:
-                        yield f
-                    ring_buffer.clear()
-
-            else:
-                print('lets yield')
-                yield frame
-                ring_buffer.append((frame, is_speech))
-                num_unvoiced = len(
-                    [f for f, speech in ring_buffer if not speech])
-                if num_unvoiced > ratio * ring_buffer.maxlen:
-                    triggered = False
-                    yield None
-                    ring_buffer.clear()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['CONVERTED'] = os.path.join(
+    app.config['UPLOAD_FOLDER'], 'converted')
+app.config['RAW'] = os.path.join(app.config['UPLOAD_FOLDER'], 'raw')
+CORS(app)
 
 
 def create_model_from_config(config: Config) -> deepspeech.Model:
@@ -159,34 +38,79 @@ def load_config() -> Config:
         return json.load(json_file)['config']
 
 
-def process_audio(frames):
-    # Start audio with VAD
-    frames_collected = vad_audio.vad_collector(frames=frames)
-    # Stream from microphone to DeepSpeech using VAD
-    stream_context = model.createStream()
-    for frame in frames_collected:
-        if frame is not None:
-            logging.debug("streaming frame")
-            model.feedAudioContent(
-                stream_context, np.frombuffer(frame, np.int16))
-        else:
-            logging.debug("end utterence")
-            text = model.finishStreamWithMetadata(stream_context)
-            # for item in text.items:
-            #     print(item.character, item.start_time, item.timestep)
-            text = "".join([item.character for item in text.items])
-            # callback(text)
-            print("text", text)
-            stream_context = model.createStream()
-    # print("this finished?")
+def normalize_file(file_name: str) -> str:
+    print('here we go')
+    full_path = os.path.join(
+        app.config['RAW'], file_name)
+    destination = os.path.join(app.config['CONVERTED'], file_name)
+    (ffmpeg.input(full_path)
+     .output(destination, acodec='pcm_s16le', ac=1, ar='16k')
+     .overwrite_output()
+     .run())
+
+    return destination
+
+
+def process_transcript(transcript):
+    items = transcript.items  # character, start_time, timestep
+    result = []
+    word_start = 0
+    for i, item in enumerate(items):
+        if item.character == ' ':
+            start_time = items[word_start].start_time
+            end_time = item.start_time
+            word = ''.join([curr.character for curr in items[word_start:i]])
+            result.append(
+                {"word": word, "start-time": start_time, "end-time": end_time})
+            result.append({"word": " ", "start-time": end_time,
+                           "end-time": items[i+1].start_time if i < len(items) else None})
+            word_start = i + 1
+    return result
+
+
+def transcribe(file_name: str):
+    fs, audio = wav.read(file_name)
+    print("Loaded file")
+    print("Starting transcription...")
+    processed_data = model.sttWithMetadata(audio)
+    os.remove(file_name)
+    return processed_data
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print(request.files)
+    if len(request.files) == 0:
+        return jsonify(
+            error="No file in request"
+        ), 400
+    for fi in request.files:
+        file = request.files[fi]
+        if file and allowed_file(file.filename):
+            file_name = secure_filename(file.filename)
+            raw_file = os.path.join(
+                app.config['RAW'], file_name)
+            file.save(raw_file)
+            file_name = normalize_file(file_name)
+            transcript = transcribe(file_name)
+            # print(''.join([item.character for item in transcript.items]))
+            # [print(item.character, item.start_time, item.timestep)
+            #  for item in transcription.items]
+            transcript = process_transcript(transcript)
+            os.remove(raw_file)
+            return jsonify(
+                transcript
+            ), 201
+    return jsonify(message="no file"), 400
 
 
 config = load_config()
 model = create_model_from_config(config)
-vad_audio = VADAudio(config["VAD_AGGRESSIVENESS"])
-# _ = socket.start_background_task(process_audio)
-
 
 if __name__ == "__main__":
-    socket.run(
-        app, debug=True, use_reloader=True)
+    app.run('localhost', 5000, debug=True, use_reloader=True)
